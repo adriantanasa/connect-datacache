473de1e0aa9bb07cbbee6fd0f9bd9630a10eb931
Adrian Tanasa <adrian_tanasa@yahoo.com>
2016-10-05 12:00:34 +0100

    Deploying implementation and tests for connect-datacache

--- README.md
+++ README.md
@@ -1,1 +1,151 @@
 # connect-datacache
+
+NodeJS express-session storage for IBM DataCache service. 
+
+## Setup
+
+```
+npm install connect-datacache
+```
+
+Using the DataCache storage connector:
+
+```javascript
+var session = require('express-session');
+var DataCacheStore = require('connect-datacache')(session);
+
+app.use(session({
+    store: new DataCacheStore(options),
+    secret: 'keyboard cat'
+}));
+```
+
+Standard usage for Bluemix environment/dev environment - with fallback on MemoryStore:
+
+```javascript
+dcStore = null;
+try { 
+    // by default is looking into bluemix cfenv services
+    dcStore = new DataCacheStore();
+} catch (err) {
+    // log fallback on memory store for no DataCache service linked to app
+}
+
+app.use(session({
+    store: dcStore,
+    secret: 'keyboard cat'
+}));
+```
+
+## Storage Options
+
+Bellow is an example with the full list of parameters - default values for optional ones:
+
+```javascript
+var store = new DataCacheStore(
+    // required parameters when no custom client provided
+    restResource: 'http://dcsdomain.bluemix.net/resources/datacaches/{gridName}',
+    restResourceSecure: 'https://dcsdomain.bluemix.net/resources/datacaches/{gridName}',
+    gridName: '{gridName}',
+    username: '{username}',
+    password: '{password}',
+    // optional parameters - default values
+    mapName: '{gridName}',
+    eviction: 'LUT',
+    locking: 'optimistic',
+    contentType: 'application/json',
+    secure: true,
+    ttl: 3600,
+    client: null
+);
+
+```
+
+### Bluemix environment
+
+The datacache client is looking first for DataCache service cfenv values. For the Bluemix NodeJS app with a DataCache service associated the required parameters are read from ENV variables (credentials): 
+
+Environment Variables > VCAP_SERVICES
+
+```json
+{
+    "system_env_json": {
+      "VCAP_SERVICES": {
+         "DataCache-dedicated": [
+            {
+               "credentials": {
+                 "catalogEndPoint": "...",
+                 "restResource": "http://ip-numeric/resources/datacaches/SYS_GENERATED_GRIDNAME",
+                 "restResourceSecure": "https://sdomain.bluemix.net/resources/datacaches/SYS_GENERATED_GRIDNAME",
+                 "gridName": "SYS_GENERATED_GRIDNAME",
+                 "username": "sysGeneratedUsername",
+                 "password": "sysGeneratedPass"
+               },
+               "name": "datacache-service-name",
+               "tags": []
+            }
+         ]
+      }
+   },
+}
+```
+
+### restResources/restResourceSecure
+defaults: VCAP_SERVICES credentials values
+
+Depending on the "secure" value, one of them is required if not found in ENV variables by cfenv.
+
+
+### mapName
+For a Bluemix application it is required to have the same value as for "gridName". A resource is identified with a complete URI as:
+
+http://secure.domain/resources/datacaches/SYS_GENERATED_GRIDNAME/MAP_NAME.EVICTION.LOCK/SESSION_KEY
+
+ex:
+https://ecaas3.w3ibm.bluemix.net/resources/datacaches/Ae7hjz7tQjuxF44ncLAvuQGH/Ae7hjz7tQjuxF44ncLAvuQGH.LUT.O/sess:t7OQGXZ3x8TNp269-lf-wsdnUBx5OcU6
+
+For non-Bluemix environments can be customized as a namespace for data.
+
+### eviction
+- 'LUT' - default
+- 'NONE'
+- 'LAT'
+
+### locking
+- 'optimistic' - default
+- 'pessimistic'
+
+### contentType
+- 'application/json' - default - turns on the JSON encoder/decoder for session data
+- other - saves session data as plain text
+
+### secure
+- true - default - uses 'restResourceSecure' as store entrypoint
+- false  - uses 'restResource' as store entrypoint
+
+### ttl
+- session/storage time to live - overrides the cookie maxAge value if present
+
+### client
+- offers option to pass an instance of DataCacheClient or inteface matching object -> turns optional all the required parameters/credentials
+
+## Contributing
+
+- PR code needs to pass the lint check and unit test
+
+```
+npm test
+```
+- PR code should be covered by UT
+
+```
+npm run coverage
+```
+
+### Resources
+
+- http://www.ibm.com/support/knowledgecenter/SSTVLU_8.6.0/com.ibm.websphere.extremescale.doc/tdevrest.html
+- https://console.ng.bluemix.net/docs/services/DataCache/index.html#datac001
+
+### Attributions
+- The connect-datacache code is based on implementation from other express-session storages as: connect-redis
---
+++ test/connect-datacacheSpec.js
@@ -0,0 +1,214 @@
+'use strict';
+
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    DataCacheClient = require('../lib/datacache-client'),
+    session = require('express-session'),
+    DataCacheStore = require('../lib/connect-datacache')(session);
+
+describe('Testsuite - DataCacheClient', function() {
+    var dcClientGetStub,
+        dcClientPutStub,
+        dcClientDestroyStub;
+
+    var goodClientParams = {
+        username: 'usertest',
+        password: 'passwordtest',
+        restResourceSecure: 'https://restSecureDomain.tst/resources/datacaches/gridNameTest',
+        gridName: 'gridNameTest'
+    };
+
+    describe('TestSuite - DataCacheClient', function() {
+        beforeEach(function() {
+            dcClientGetStub = sinon.stub(DataCacheClient.prototype, 'get');
+            dcClientPutStub = sinon.stub(DataCacheClient.prototype, 'put');
+            dcClientDestroyStub = sinon.stub(DataCacheClient.prototype, 'destroy');
+        });
+
+        afterEach(function() {
+            dcClientGetStub.restore();
+            dcClientPutStub.restore();
+            dcClientDestroyStub.restore();
+        });
+
+        it('Testcase - failing constructor - required params missing', function() {
+            var error = null;
+            try {
+                var store = new DataCacheStore();
+            } catch (err) {
+                error = err;
+            }
+            expect(error).not.to.be.null;
+            expect(store).not.to.be.ok;
+        });
+
+        it('Testcase - constructor - spy client', function() {
+            var store = new DataCacheStore({
+                client: sinon.spy()
+            });
+
+            expect(store).to.be.ok;
+        });
+
+        it('Testcase - constructor - passing params to client', function() {
+            var store = new DataCacheStore(goodClientParams);
+            expect(store).to.be.ok;
+            expect(store.client).to.be.ok;
+            expect(store.client.restURI).to.equal('https://restSecureDomain.tst/resources/datacaches');
+        });
+
+        it('Testcase - store get - success', function(done) {
+            var goodData = {data: 'data'};
+            var stubCallback = sinon.spy();
+            dcClientGetStub.returns(Promise.resolve({body: goodData}));
+            var store = new DataCacheStore(goodClientParams);
+            store.get('key', stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(stubCallback.calledWith(null, {data: 'data'})).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store get - regular failure', function(done) {
+            var error = {statusCode: 500};
+            dcClientGetStub.returns(Promise.reject(error));
+            var stubCallback = sinon.spy();
+            var store = new DataCacheStore(goodClientParams);
+
+            store.get('key', stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(stubCallback.calledWith(error)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store get - 404 failure', function(done) {
+            var error = {statusCode: 404};
+            dcClientGetStub.returns(Promise.reject(error));
+            var stubCallback = sinon.spy();
+            var store = new DataCacheStore(goodClientParams);
+
+            store.get('key', stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(stubCallback.called).to.equal(true);
+                expect(stubCallback.calledWith(error)).to.equal(false);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store put - success', function(done) {
+            var goodData = {data: 'data'};
+            var stubCallback = sinon.spy();
+            dcClientPutStub.returns(Promise.resolve({statusCode: 200, body: ''}));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {ttl: 100}));
+            store.set('key', goodData,  stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientPutStub.calledWith('sess:key', goodData, 100)).to.be.true;
+                expect(stubCallback.calledWith(null, null)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store put - failure', function(done) {
+            var goodData = {data: 'data'};
+            var error = {statusCode: 500};
+            var stubCallback = sinon.spy();
+            dcClientPutStub.returns(Promise.reject(error));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {ttl: 100}));
+            store.set('key', goodData,  stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientPutStub.calledWith('sess:key', goodData, 100)).to.be.true;
+                expect(stubCallback.calledWith(error)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store destroy - success', function(done) {
+            var stubCallback = sinon.spy();
+            dcClientDestroyStub.returns(Promise.resolve());
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {prefix: 'myprefix:'}));
+            store.destroy('key',  stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientDestroyStub.calledWith('myprefix:key')).to.be.true;
+                // callback not called for successfull destroy of sess data
+                expect(stubCallback.called).to.equal(false);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store destroy - failure', function(done) {
+            var error = {statusCode: 500};
+            var stubCallback = sinon.spy();
+            dcClientDestroyStub.returns(Promise.reject(error));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {prefix: 'myprefix:'}));
+            store.destroy('key',  stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientDestroyStub.calledWith('myprefix:key')).to.be.true;
+                expect(stubCallback.calledWith(error)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store touch - success', function(done) {
+            var goodData = {data: 'data'};
+            var stubCallback = sinon.spy();
+            var session = {cookie: {}};
+            dcClientGetStub.returns(Promise.resolve({body: goodData}));
+            dcClientPutStub.returns(Promise.resolve({statusCode: 200, body: ''}));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {ttl: 100}));
+            store.touch('key', session, stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(dcClientPutStub.calledWith('sess:key', Object.assign({}, session, goodData), 100)).to.be.true;
+                expect(stubCallback.calledWith(null, null)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store touch - update error', function(done) {
+            var error = {statusCode: 500};
+            var goodData = {data: 'data'};
+            var stubCallback = sinon.spy();
+            var session = {cookie: {}};
+            dcClientGetStub.returns(Promise.resolve({body: goodData}));
+            dcClientPutStub.returns(Promise.reject(error));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {ttl: 100}));
+            store.touch('key', session, stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(dcClientPutStub.calledWith('sess:key', Object.assign({}, session, goodData), 100)).to.be.true;
+                expect(stubCallback.calledWith(error)).to.equal(true);
+                done();
+            }, 0);
+        });
+
+        it('Testcase - store touch - get error', function(done) {
+            var error = {statusCode: 500};
+            var stubCallback = sinon.spy();
+            var session = {cookie: {}};
+            dcClientGetStub.returns(Promise.reject(error));
+            dcClientPutStub.returns(Promise.reject(error));
+            var store = new DataCacheStore(Object.assign({}, goodClientParams, {ttl: 100}));
+            store.touch('key', session, stubCallback);
+
+            setTimeout(function() {
+                expect(dcClientGetStub.calledWith('sess:key')).to.be.true;
+                expect(dcClientPutStub.calledWith('sess:key')).to.be.false;
+                expect(stubCallback.calledWith(error)).to.equal(true);
+                done();
+            }, 0);
+        });
+    });
+});
